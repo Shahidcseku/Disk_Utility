5b9ad6881ed5f7747a7b98741e4871e697e77875
Shahidul Islam <shahid.sm35@gmail.com>
2017-10-29 02:07:52 +0600

    - Second build.. Shamim arif sir - Info Display - Rename - Format
    Signed-off-by: Shahidul Islam <shahid.sm35@gmail.com>

---
+++ OS/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace OS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.create_panel = new System.Windows.Forms.Panel();
+            this.dispBtn = new MetroFramework.Controls.MetroTextBox.MetroTextButton();
+            this.listDisk = new MetroFramework.Controls.MetroComboBox();
+            this.details = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.progressbar = new CircularProgressBar.CircularProgressBar();
+            this.content = new System.Windows.Forms.Panel();
+            this.formatBtn = new MetroFramework.Controls.MetroTextBox.MetroTextButton();
+            this.renameBtn = new MetroFramework.Controls.MetroTextBox.MetroTextButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.create_panel.SuspendLayout();
+            this.content.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Disk Utility";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::OS.Properties.Resources.if_6___Cross_1815573;
+            this.pictureBox1.Location = new System.Drawing.Point(679, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.close_Click);
+            // 
+            // create_panel
+            // 
+            this.create_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(37)))), ((int)(((byte)(59)))));
+            this.create_panel.Controls.Add(this.renameBtn);
+            this.create_panel.Controls.Add(this.formatBtn);
+            this.create_panel.Controls.Add(this.dispBtn);
+            this.create_panel.Controls.Add(this.listDisk);
+            this.create_panel.Controls.Add(this.details);
+            this.create_panel.Controls.Add(this.name);
+            this.create_panel.Controls.Add(this.progressbar);
+            this.create_panel.Location = new System.Drawing.Point(0, 0);
+            this.create_panel.Name = "create_panel";
+            this.create_panel.Size = new System.Drawing.Size(746, 338);
+            this.create_panel.TabIndex = 3;
+            this.create_panel.Tag = "2";
+            this.create_panel.Paint += new System.Windows.Forms.PaintEventHandler(this.create_panel_Paint);
+            // 
+            // dispBtn
+            // 
+            this.dispBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.dispBtn.Image = null;
+            this.dispBtn.Location = new System.Drawing.Point(562, 9);
+            this.dispBtn.Name = "dispBtn";
+            this.dispBtn.Size = new System.Drawing.Size(136, 35);
+            this.dispBtn.TabIndex = 4;
+            this.dispBtn.Text = "Show Info";
+            this.dispBtn.UseSelectable = true;
+            this.dispBtn.UseVisualStyleBackColor = true;
+            this.dispBtn.Click += new System.EventHandler(this.dispBtn_Click);
+            // 
+            // listDisk
+            // 
+            this.listDisk.FormattingEnabled = true;
+            this.listDisk.ItemHeight = 23;
+            this.listDisk.Location = new System.Drawing.Point(12, 9);
+            this.listDisk.Name = "listDisk";
+            this.listDisk.Size = new System.Drawing.Size(523, 29);
+            this.listDisk.TabIndex = 3;
+            this.listDisk.UseSelectable = true;
+            // 
+            // details
+            // 
+            this.details.AutoSize = true;
+            this.details.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.details.ForeColor = System.Drawing.Color.Gold;
+            this.details.Location = new System.Drawing.Point(36, 214);
+            this.details.Name = "details";
+            this.details.Size = new System.Drawing.Size(51, 17);
+            this.details.TabIndex = 2;
+            this.details.Text = "Details";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.name.Location = new System.Drawing.Point(35, 193);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(66, 22);
+            this.name.TabIndex = 0;
+            this.name.Text = "Name";
+            // 
+            // progressbar
+            // 
+            this.progressbar.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.progressbar.AnimationSpeed = 500;
+            this.progressbar.BackColor = System.Drawing.Color.Transparent;
+            this.progressbar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progressbar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.progressbar.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.progressbar.InnerMargin = 2;
+            this.progressbar.InnerWidth = -1;
+            this.progressbar.Location = new System.Drawing.Point(12, 44);
+            this.progressbar.MarqueeAnimationSpeed = 2000;
+            this.progressbar.Name = "progressbar";
+            this.progressbar.OuterColor = System.Drawing.Color.Navy;
+            this.progressbar.OuterMargin = -25;
+            this.progressbar.OuterWidth = 20;
+            this.progressbar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.progressbar.ProgressWidth = 15;
+            this.progressbar.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.progressbar.Size = new System.Drawing.Size(133, 130);
+            this.progressbar.StartAngle = 270;
+            this.progressbar.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.progressbar.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.progressbar.SubscriptText = "";
+            this.progressbar.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.progressbar.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.progressbar.SuperscriptText = "";
+            this.progressbar.TabIndex = 1;
+            this.progressbar.Text = "0%";
+            this.progressbar.TextMargin = new System.Windows.Forms.Padding(4, 4, 0, 0);
+            this.progressbar.Value = 68;
+            // 
+            // content
+            // 
+            this.content.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.content.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(37)))), ((int)(((byte)(59)))));
+            this.content.Controls.Add(this.create_panel);
+            this.content.Location = new System.Drawing.Point(0, 43);
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(746, 338);
+            this.content.TabIndex = 2;
+            this.content.Tag = "1";
+            // 
+            // formatBtn
+            // 
+            this.formatBtn.Image = null;
+            this.formatBtn.Location = new System.Drawing.Point(562, 99);
+            this.formatBtn.Name = "formatBtn";
+            this.formatBtn.Size = new System.Drawing.Size(136, 35);
+            this.formatBtn.TabIndex = 5;
+            this.formatBtn.Text = "Format Drive";
+            this.formatBtn.UseSelectable = true;
+            this.formatBtn.UseVisualStyleBackColor = true;
+            this.formatBtn.Click += new System.EventHandler(this.formatBtn_Click);
+            // 
+            // renameBtn
+            // 
+            this.renameBtn.Image = null;
+            this.renameBtn.Location = new System.Drawing.Point(562, 140);
+            this.renameBtn.Name = "renameBtn";
+            this.renameBtn.Size = new System.Drawing.Size(136, 34);
+            this.renameBtn.TabIndex = 6;
+            this.renameBtn.Text = "Rename Drive";
+            this.renameBtn.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.renameBtn.UseSelectable = true;
+            this.renameBtn.UseVisualStyleBackColor = true;
+            this.renameBtn.Click += new System.EventHandler(this.renameBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.ClientSize = new System.Drawing.Size(746, 381);
+            this.Controls.Add(this.content);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.create_panel.ResumeLayout(false);
+            this.create_panel.PerformLayout();
+            this.content.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel content;
+        private CircularProgressBar.CircularProgressBar progressbar;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label details;
+        private System.Windows.Forms.Panel create_panel;
+        private MetroFramework.Controls.MetroComboBox listDisk;
+        private MetroFramework.Controls.MetroTextBox.MetroTextButton dispBtn;
+        private MetroFramework.Controls.MetroTextBox.MetroTextButton formatBtn;
+        private MetroFramework.Controls.MetroTextBox.MetroTextButton renameBtn;
+    }
+}
+
---
+++ OS/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using OS;
+
+namespace OS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getDrives();
+        }
+        // Drive information acquisition
+        private void getDrives()
+        {
+            string info_sum = "";
+            string[] drives = Directory.GetLogicalDrives();
+            foreach (string item in drives)
+            {
+
+                info_sum += getLabel(item);
+                if(getTotalSpace(item)>0)
+                {
+                    listDisk.Items.Add(item);
+                }
+                
+
+            }
+            // For null selection handling 
+            listDisk.SelectedIndex = 0;
+            
+
+        }
+
+        // Drive Processing
+
+        private string showInfo(string item)
+        {
+
+            string available_space = "", total_space = "", type = "", file_system = "", label = "";
+            string selected_drive = item;
+            int percentage = 0;
+            try
+            {
+                DriveInfo info = new DriveInfo(selected_drive);
+                available_space = info.AvailableFreeSpace.ToString();
+                total_space = info.TotalFreeSpace.ToString();
+                type = info.DriveType.ToString();
+                file_system = info.DriveFormat.ToString();
+                label = info.VolumeLabel.ToString();
+
+                percentage = int.Parse(available_space) / int.Parse(total_space) * 100;
+            
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return "Free space: " + available_space + "\nTotal Sapace: " + total_space + "\nDrive type: " + type + "\nFile System: " + file_system;
+        }
+
+        private string getLabel(string drive)
+        {
+            string result = "";
+            DriveInfo info = new DriveInfo(drive);
+            try
+            {
+                result = info.VolumeLabel.ToString();
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return result;
+        }
+        private string getFileSystem(string drive)
+        {
+            string result = "";
+            DriveInfo info = new DriveInfo(drive);
+            try
+            {
+                result = info.DriveFormat.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+        private double getTotalSpace(string drive)
+        {
+            double result = 0;
+            DriveInfo info = new DriveInfo(drive);
+            try
+            {
+                result = double.Parse(info.TotalSize.ToString());
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+        private double getAvailableSpace(string drive)
+        {
+            double result = 0;
+            DriveInfo info = new DriveInfo(drive);
+            try
+            {
+                result = double.Parse(info.AvailableFreeSpace.ToString());
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+        private string getType(string drive)
+        {
+            string result = "";
+            DriveInfo info = new DriveInfo(drive);
+            try
+            {
+                result = info.DriveType.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+        }
+
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Drag controll
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+        // Drag control end
+
+
+        private void create_panel_Paint(object sender, PaintEventArgs e)
+        {
+            /// VVI
+            /// don't delete.. used for design
+        }
+
+        // Display Drive
+        private void dispBtn_Click(object sender, EventArgs e)
+        {
+            string item = listDisk.SelectedItem.ToString();
+            name.Text = getLabel(item);
+            details.Text = showInfo(item);
+
+            double percentage = (getAvailableSpace(item) / getTotalSpace(item));
+            percentage = percentage * 100;
+            int val = Convert.ToInt32(percentage);
+            progressbar.Value = val;
+            progressbar.Text = val + " %";
+            progressbar.Update();
+        }
+
+        // Format drive
+        private void formatBtn_Click(object sender, EventArgs e)
+        {
+            string item = listDisk.SelectedItem.ToString();
+            char driveLetter = item[0];
+            DriveInfo d = new DriveInfo(item);
+            //Console.Write(driveLetter);
+
+            DialogResult dr = MessageBox.Show("Are you sure to format this Drive?", "Confirmation", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                if (d.IsReady && d.DriveType == DriveType.Removable)
+                {
+                    bool res = DriveManager.FormatDrive(driveLetter, getLabel(item));
+                    if (res)
+                    {
+                        MessageBox.Show("Format completed!", "Operation Successfull");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Can't Format Drive!", "Operation Failed");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Drive formatting blocked. Select a removable drive!");
+                }
+            }
+            else if (dr == DialogResult.No)
+            {
+                //Nothing to do
+            }
+
+           
+        }
+        // Rename drive
+        public bool SetVolumeLabel(DriveInfo d,string newLabel)
+        {
+            if (d.IsReady && d.DriveType == DriveType.Removable)
+            {
+                d.VolumeLabel = newLabel;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void renameBtn_Click(object sender, EventArgs e)
+        {
+            string item = listDisk.SelectedItem.ToString();
+            DriveInfo d = new DriveInfo(item);
+
+            ShowDialog("Enter label","Change label");
+            bool res = SetVolumeLabel(d, "Noor");
+            if(res)
+            {
+                MessageBox.Show("Drive Rename Successfull!");
+            }
+            else
+            {
+                MessageBox.Show("Drive Rename blocked. Select a removable drive!");
+
+            }
+
+        }
+
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 300 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+
+    }
+
+}
